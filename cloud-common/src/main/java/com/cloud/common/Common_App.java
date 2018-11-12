4d4fdb8caffaf1fb7181c1d7c6a98cb60a5602c1
limin02 <limin02@wondersgroup.com>
2018-11-12 17:35:40 +0800

    add euerka module

---
+++ cloud-common/src/main/java/com/cloud/common/Common_App.java
@@ -0,0 +1,13 @@
+package com.cloud.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Common_App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Common_App.class);
+    }
+
+}
--- could-eureka-7001/src/main/java/com/cloud/eureka/Eureka_7001_App.java
+++ could-eureka-7001/src/main/java/com/cloud/eureka/Eureka_7001_App.java
@@ -2,8 +2,10 @@ package com.cloud.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class Eureka_7001_App {
 
     public static void main(String[] args) {
