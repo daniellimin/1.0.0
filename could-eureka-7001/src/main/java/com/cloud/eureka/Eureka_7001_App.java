964a53c6e4d1dabbbe2011f3456f9b36a00cbdc3
limin02 <limin02@wondersgroup.com>
2018-11-12 11:36:17 +0800

    create project

---
+++ could-eureka-7001/src/main/java/com/cloud/eureka/Eureka_7001_App.java
@@ -0,0 +1,12 @@
+package com.cloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eureka_7001_App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eureka_7001_App.class);
+    }
+}
